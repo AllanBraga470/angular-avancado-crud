6b562e6ac2439ad24026e5f5341cc3979abd44a6
Allan <allanbraga470@gmail.com>
2019-08-30 20:28:06 +0100

    Source code

---
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Category } from '../shared/category.model';
+import { CategoryService } from './../shared/category.service';
+
+@Component({
+  selector: 'app-category-list',
+  templateUrl: './category-list.component.html',
+  styleUrls: ['./category-list.component.css']
+})
+export class CategoryListComponent implements OnInit {
+
+  categories: Category[] = [];
+
+  constructor(private categoryService: CategoryService) { }
+
+  ngOnInit() {
+    this.categoryService.getAll().subscribe(
+      categories => this.categories = categories,
+      error => alert('Error to load categories list')
+      );
+  }
+
+  deleteCategory(category: Category) {
+    const mustDelete = confirm('Confirm delete item?');
+    if (mustDelete) {
+      this.categoryService.delete(category.id).subscribe(
+        () => this.categories = this.categories.filter(element => element !== category),
+        () => alert('Error to delete category')
+      );
+    }
+  }
+}
